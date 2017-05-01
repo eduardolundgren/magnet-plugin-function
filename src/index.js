adccc55d3b9c42bb6bf15edec26fac6ea4313c9a
Ivan Santos <pragmaticivan@gmail.com>
2017-05-01 10:54:22 -0700

    Imported from https://github.com/wedeploy/magnet/commit/8d898517e5e2fc6b005b513962fc8a6b47ef3f49

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import {assertDefAndNotNull, assertString} from 'metal-assertions';
+import {isFunction, isObject} from 'metal';
+
+const isMultiple = module => {
+  return isObject(module.route) && module.route.multiple;
+};
+
+export default {
+  test(module, filename, magnet) {
+    return (
+      !isMultiple(module) &&
+      isObject(module.route) &&
+      isFunction(module.default)
+    );
+  },
+
+  register(module, filename, magnet) {
+    const path = module.route.path;
+    const method = module.route.method || 'get';
+    const type = module.route.type || 'html';
+    const fileshort = filename.substring(
+      magnet.getServerDistDirectory().length
+    );
+
+    assertString(
+      method,
+      `Route configuration method must be a string, ` + `check ${fileshort}.`
+    );
+    assertDefAndNotNull(
+      path,
+      `Route configuration path must be specified, ` + `check ${fileshort}.`
+    );
+
+    const app = magnet.getServer().getEngine();
+
+    app[method.toLowerCase()](path, async (req, res, next) => {
+      try {
+        let result = await module.default.call(module.default, req, res, next);
+        if (!res.headersSent) {
+          res.type(type).send(result);
+        }
+      } catch (error) {
+        next(error);
+      }
+    });
+  },
+};
